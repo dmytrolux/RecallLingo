e73361d36ea01e035628eefecec17160c0c8b285
Dmytro Pryshlyak <salokpi@gmail.com>
2023-04-03 20:04:07 +0300

    Initial Commit

---
+++ RecallLingo/RecallLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallLingoApp.swift
+//  RecallLingo
+//
+//  Created by Pryshliak Dmytro on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecallLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
